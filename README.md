3a6f5e94901bd6d24fc21f89d4324c7b26647150
Erik Zimmermann <mail@erikzimmermann.de>
2023-05-20 14:28:34 +0200

    Fix omp_mm.cpp

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # multi-core-matrix-multiplication
 
 Build c file: `mpic++ -fopenmp test-matrix.cpp mpi_mm.cpp omp_mm.cpp mm.cpp -o test-matrix`  
-Run c file for MPI: `mpirun -np 17 ./test-matrix mpi`  
+Run c file for MPI: `mpirun -np 17 ./test-matrix 2000 mpi`  
 
 Allocate palma node: `salloc -N1 --ntasks-per-node=17 -p normal -t 01:00:00 --exclusive`  
 Allocate palma node: `salloc -N4 --ntasks-per-node=17 -p normal -t 02:00:00 --exclusive`  
--- omp_mm.cpp
+++ omp_mm.cpp
@@ -2,45 +2,49 @@
 #include <omp.h>
 
 void multiplyMatrixOMP(float *a, float *b, float *c,
-                    int crow, int ccol,
-                    int arow, int acol,
-                    int brow, int bcol,
-                    int l, int m, int n, int N) {
+                       int crow, int ccol,
+                       int arow, int acol,
+                       int brow, int bcol,
+                       int l, int m, int n, int N) {
     int lhalf[3], mhalf[3], nhalf[3];
-    int i, j, k;
-    float *aptr, *bptr, *cptr;
 
     if (m * n > 32768) {
         lhalf[0] = 0; lhalf[1] = l/2; lhalf[2] = l - l/2;
         mhalf[0] = 0; mhalf[1] = m/2; mhalf[2] = l - m/2;
         nhalf[0] = 0; nhalf[1] = n/2; nhalf[2] = l - n/2;
 
-        #pragma omp parallel default(shared)
-        #pragma omp single
-        {
-            for (i = 0; i < 2; i++) {
-                for (j = 0; j < 2; j++) {
-                    for (k = 0; k < 2; k++) {
-                        #pragma omp task
-                        multiplyMatrixOMP(a, b, c,
-                                       crow + lhalf[i], ccol + mhalf[j],
-                                       arow + lhalf[i], acol + mhalf[k],
-                                       brow + mhalf[k], bcol + nhalf[j],
-                                       lhalf[i + 1], mhalf[k + 1], nhalf[k + 1], N);
-                    }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    multiplyMatrixOMP(a, b, c,
+                                      crow + lhalf[i], ccol + mhalf[j],
+                                      arow + lhalf[i], acol + mhalf[k],
+                                      brow + mhalf[k], bcol + nhalf[j],
+                                      lhalf[i + 1], mhalf[k + 1], nhalf[k + 1], N);
                 }
             }
         }
     } else {
-        for (i = 0; i < l; i++) {
-            for (j = 0; j < n; j++) {
-                cptr = c + (crow + i) * N + (ccol + j);
-                aptr = a + (arow + i) * N + acol;
-                bptr = b + brow * N + bcol + j;
+        #pragma omp parallel
+        {
+            #pragma omp for
+            for (int i = 0; i < l; i++) {
+                for (int j = 0; j < n; j++) {
+                    float *aptr = a + (arow + i) * N + acol;
+                    float *bptr = b + brow * N + bcol + j;
+                    float *cptr = c + (crow + i) * N + (ccol + j);
 
-                for (k = 0; k < m; k++) {
-                    *cptr += *(aptr++) * *bptr;
-                    bptr += N;
+                    float temp = 0.0f;
+                    #pragma omp simd reduction(+:temp)
+                    for (int k = 0; k < m; k++) {
+                        temp += *(aptr++) * *bptr;
+                        bptr += N;
+                    }
+
+                    #pragma omp critical
+                    {
+                        *cptr += temp;
+                    }
                 }
             }
         }
