2aa8e6fa2029de5b7c437dec553bd4ffe19d3f26
Erik Zimmermann <mail@erikzimmermann.de>
2023-05-23 14:26:34 +0200

    Upload omp block_size test

--- omp_mm.cpp
+++ omp_mm.cpp
@@ -1,6 +1,8 @@
 #include "omp_mm.h"
 #include <omp.h>
 #include <math.h>
+#include <chrono>
+#include <iostream>
 
 //const int block_size = 32;
 
@@ -33,13 +35,62 @@ void multiplyMatrixCannon(float *a, float *b, float *c, int n, int block_size) {
     }
 }
 
+int getIdOfMax(double *arr, int size) {
+    double max = 0;
+    int id = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+            id = i;
+        }
+    }
+
+    return id;
+}
+
+int getIdOfMin(double *arr, int size) {
+    double min = 1000000;
+    int id = 0;
+
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+            id = i;
+        }
+    }
+
+    return id;
+}
+
 void multiplyMatrixOMP(float *a, float *b, float *c, int n, int threads) {
     if (threads > 0) omp_set_num_threads(threads);
     else omp_set_num_threads(omp_get_max_threads());
 
-    auto sizes = new int[10] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+    auto sizes = new int[8] { 1, 2, 4, 8, 16, 32, 64, 128 };
 
-    for (int i = 0; i < 10; ++i) {
-        multiplyMatrixCannon(a, b, c, n, sizes[i]);
+    for (int i = 0; i < 8; ++i) {
+        auto timings = new double[7];
+        for (int j = 0; j < 7; ++j) {
+            auto start_time = std::chrono::system_clock::now();
+            multiplyMatrixCannon(a, b, c, n, sizes[i]);
+            auto end_time = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+            double time = elapsed_seconds.count();
+            timings[j] = time;
+        }
+
+        int min = getIdOfMin(&timings[0], 7);
+        int max = getIdOfMax(&timings[0], 7);
+
+        double sum = 0;
+        for (int j = 0; j < 7; ++j) {
+            if (j != min && j != max) {
+                sum += timings[j];
+            }
+        }
+        sum /= 5;
+
+        std::cout << sizes[i] << ": " << sum << std::endl;
     }
 }
